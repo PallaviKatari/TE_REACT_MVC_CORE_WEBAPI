cddd377c3abd39c74a2a70b98a66fc94040930d3
PallaviKatari <pallavikatari@gmail.com>
2022-10-14 14:06:03 +0530

    commit_14/10/22

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import React from 'react';
 import logo from './react.png';
 import './App.css';
+import NavBar from './Components/NavBar';
 //FuncComp - alias - start with an Uppercase
 import FuncComp from './Components/Functional';
 import ClassComp from './Components/ClassComponent';
+//Configure the routing paths 
+import { Route,Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <FuncComp></FuncComp>
-      <hr>
-      </hr>
-      <ClassComp></ClassComp>
+      <NavBar/>
+     <Routes>
+      <Route path="funcom" element={<FuncComp/>}></Route>
+      <Route path="classcom" element={<ClassComp/>}></Route>
+     </Routes>
     </div>
   );
 }
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../Assets/react.png';
+
+//class -> className
+export default function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <img src={logo} style={{height:50,width:50,padding:10}}></img>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="funcom">Functional Component</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="classcom">Class Component</Link>
+                </li>
+
+            </ul>
+        </nav>
+    )
+}
